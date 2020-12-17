c43aeb754379227243855de160740ee4cf22af36
bluewood <bluewood.truth@gmail.com>
2020-12-17 15:55:29 +0900

    feat: InputView에 사용자 입력을 받아 반환하는 기능 추가

--- src/main/java/lottogame/view/InputView.java
+++ src/main/java/lottogame/view/InputView.java
@@ -4,4 +4,8 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
+
+    public static String getInput(){
+        return scanner.nextLine();
+    }
 }
